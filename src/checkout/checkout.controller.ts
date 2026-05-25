33d1ffa9b6b18a1bc772802c6e2f47fe816ab0f0
prashant-shrestha <peashant907@gmail.com>
2026-05-25 17:26:22 +0545

    crud of order analytics and dynamic date pciker component

--- src/checkout/checkout.controller.ts
+++ src/checkout/checkout.controller.ts
@@ -1,12 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
+import { CheckoutDto } from './dto/checkout.dto';
 
 @Controller('checkout')
 export class CheckoutController {
   constructor(private readonly checkoutService: CheckoutService) {}
 
   @Post()
-  async checkout(@Body() body: { customerId: string }) {
-    return this.checkoutService.checkout(body.customerId);
+  async checkout(@Body() body: CheckoutDto) {
+    return this.checkoutService.checkout(
+      body.customerId,
+      body.shippingAddressId,
+      body.shippingAddress,
+    );
   }
 }
+
